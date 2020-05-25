a1cb05725543c1b4404e15529b4b30a7b1b10820
Christian Pallesen <chri31h3@edu.mercantec.dk>
2020-05-25 09:47:29 +0200

    Added second part of the task

--- FørsteOpgave/FørsteOpgave/Program.cs
+++ FørsteOpgave/FørsteOpgave/Program.cs
@@ -9,12 +9,17 @@ namespace FørsteOpgave
         {
             string text = "Dette burde være et spørgsmål!";
 
-            byte[] encoder = Encoding.ASCII.GetBytes(text);
+            byte[] encoder = Encoding.UTF8.GetBytes(text);
 
             foreach (byte b in encoder)
             {
                 Console.WriteLine(b);
             }
+
+
+            var str = Encoding.UTF8.GetString(encoder);
+
+            Console.WriteLine(str);
         }
     }
 }
