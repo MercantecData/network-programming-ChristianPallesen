76a637425df3c9138fec8386c8ac3db7da21c658
Christian Pallesen <chri31h3@edu.mercantec.dk>
2020-05-25 08:59:30 +0200

    Initial commit

---
+++ Encoding/Encoding/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        } 
+    }
+}
