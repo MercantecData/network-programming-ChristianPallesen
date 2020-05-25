80b6f05dc1344024740ba1cf28130db29ebd873a
Christian Pallesen <chri31h3@edu.mercantec.dk>
2020-05-25 09:35:57 +0200

    First task

---
+++ FørsteOpgave/FørsteOpgave/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace FørsteOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Dette burde være et spørgsmål!";
+
+            byte[] encoder = Encoding.ASCII.GetBytes(text);
+
+            foreach (byte b in encoder)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
