7335dc8e6450c0750b63931b1589e8b17a23779a
Christian Pallesen <chri31h3@edu.mercantec.dk>
2020-05-25 13:10:04 +0200

    Finished CallResponse task

--- CallResponseOpgave/CallResponseOpgave/Program.cs
+++ CallResponseOpgave/CallResponseOpgave/Program.cs
@@ -9,19 +9,41 @@ namespace CallResponseOpgave
     {
         static void Main(string[] args)
         {
-            TcpClient client = new TcpClient();
+            bool isActive;
 
-            int port = 13356;
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
-            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            while (isActive = true)
+            {
+                gohere:
+                TcpClient client = new TcpClient();
+                Console.WriteLine("Skriv ip'en du vil connecte til: ");
+                string ipFromUser = Console.ReadLine();
+                Console.WriteLine("Skriv hvilken port du vil connecte til: ");
+                int port = Convert.ToInt32(Console.ReadLine());
+                IPAddress ip = IPAddress.Parse(ipFromUser);
+                IPEndPoint endPoint = new IPEndPoint(ip, port);
 
-            client.Connect(endPoint);
+                client.Connect(endPoint);
+                gohere2:
+                Console.WriteLine("Du er connected til server pc'en. Skriv din tekst: ");
+                string text = Console.ReadLine();
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+                stream.Write(buffer, 0, buffer.Length);
 
-            NetworkStream stream = client.GetStream();
-            string text = "Dette er en test besked!";
-            byte[] buffer = Encoding.UTF8.GetBytes(text);
-            stream.Write(buffer, 0, buffer.Length);
-            client.Close();
+                Console.WriteLine("Din besked er sendt, hvis du vil sende en mere skriv LAST, ellers skrev NEW for at skrive til en ny person.");
+                string inputFromUser = Console.ReadLine();
+                if (inputFromUser == "LAST")
+                {
+                    goto gohere2;
+
+                } else if (inputFromUser == "NEW")
+                {
+                    goto gohere;
+                }
+                //client.Close();
+            }
+
+      
         }
     }
 }
---
+++ CallResponseOpgave2/CallResponseOpgave2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace CallResponseOpgave2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int port = 480;
+                IPAddress ip = IPAddress.Any;
+                IPEndPoint localEndpoint = new IPEndPoint(ip, port);
+                TcpListener listener = new TcpListener(localEndpoint);
+                listener.Start();
+                Console.WriteLine("Venter på forbindelser");
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[255];
+
+            while (true)
+            {
+                int numberOfBytesRead = stream.Read(buffer, 0, 255);
+                string message = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
